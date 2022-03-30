c8f6df8b493a72de9d90e803b954de4d6144b012
rishi05051997 <vrushabhdhatral10@gmail.com>
2022-03-30 19:15:40 +0530

    Feat : Products & Products Details Page with API Integration ON Live

---
+++ src/components/Products/ProductsListing.jsx
@@ -0,0 +1,61 @@
+import { useEffect } from "react";
+import { RotatingSquare } from "react-loader-spinner";
+import Footer from "../Layout/Footer";
+import { ProductsFilterAside } from "./Product-Filter-Aside/Product-Filter-Aside";
+import Product from "./Product";
+import { useData } from "../../context/data-context";
+import emptyCart from "./../../assets/empty-cart.svg"
+
+
+
+
+const ProductsListing = ({ loader }) => {
+    const {
+        state: { products }
+    } = useData();
+
+
+    useEffect(() => {
+        document.title = "APNA SHOP | Products";
+    }, []);
+    return loader ? (
+        <div className="loader-container">
+            <div className="loader-container">
+                <RotatingSquare ariaLabel="rotating-square" visible={true} color="#3b82f6" />
+            </div>
+        </div>
+    ) : (
+        <div className="filter-container">
+            <div className="header-container"></div>
+            <section className="filter-results">
+                <div className="text-3">
+                    <span className="bold">Filtered Results</span> - {products.length}
+                </div>
+
+
+                <hr className="mar-y-2" />
+            </section>
+            < ProductsFilterAside />
+            <div className="cards-section mar-y-3">
+                {
+                    products.length > 0 ? (
+                        products.map((item) => (
+                            <div key={item._id}>
+                                <Product products={item} />
+                            </div>
+                        ))
+                    ) : (
+                        <>
+                            <img src={emptyCart} alt="" />
+                            <h1 className="head-3 highlightMainText">No Products To Dispay</h1>
+                        </>
+
+                    )
+                }
+            </div>
+            <Footer />
+        </div>
+    );
+};
+
+export default ProductsListing;
---
+++ src/services/products-Service.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { API_URL } from "./apiUrl";
+
+export const fetchAllProducts = async (dispatch, setShowLoader) => {
+    try {
+        setShowLoader(true);
+        const {
+            data: { products },
+        } = await axios.get(`${API_URL}/products`);
+        if (products) {
+            dispatch({ type: "SET_PRODUCTS", payload: products });
+        }
+    } catch (err) {
+        dispatch({ type: "SHOW_TOAST", payload: "Couldn't fetch product list" });
+        console.error(err);
+    } finally {
+        setShowLoader(false);
+    }
+};
+
+export const getProductBasedOnId = async (dispatch, id) => {
+    try {
+        const { data: { product } } = await axios({
+            method: "GET",
+            url: `/api/products/${id}`,
+        })
+        debugger;
+        if (product) {
+            //// show success message
+            dispatch({ type: "GET-PRODUCT-BASED-ON-ID", payload: product })
+
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
